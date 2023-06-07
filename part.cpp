3c117f2b038d99f200a843c479ebcfe1df45bba1
abdu <sampv0166@gmail.com>
2023-06-07 22:14:33 +0400

    part command

---
+++ part.cpp
@@ -0,0 +1,8 @@
+/* PART command: user can leave the channel with this command.
+ *
+ * Syntax:          PART <channel> [<parting message>]
+ * Numeric Replies: ERR_NEEDMOREPARAMS (461)
+ *                     
+ */
+
+
