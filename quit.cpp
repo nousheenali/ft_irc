3567e49ca4ab631d4d2a65ccf029813259cc5b4c
Sumaiya Fathima <79397105+Sumi111@users.noreply.github.com>
2023-06-12 16:53:47 +0400

    adding quit msg to client

--- quit.cpp
+++ quit.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   quit.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nali <nali@42abudhabi.ae>                  +#+  +:+       +#+        */
+/*   By: sfathima <sfathima@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/01 11:19:20 by sfathima          #+#    #+#             */
-/*   Updated: 2023/06/11 10:46:46 by nali             ###   ########.fr       */
+/*   Updated: 2023/06/12 16:52:16 by sfathima         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,28 +26,9 @@ void quit(Server *server, int fd, msg_struct cmd_infos)
 	std::string msg      = cmd_infos.parameter;
     std::string quit_msg = ":" + c->get_nickname() + "!" + c->get_username() + "@localhost QUIT " + msg + "\n";
 
-    removeUserFromChannels(server, fd);
-    printRcvMsg(fd, quit_msg);
+	server->SendReply(fd, RPL_QUIT(c->get_username(), c->get_nickname(), quit_msg));
     std::cout << RED <<" *** Connection Closed by Client on socket " << fd << " *** \n" << RESET ;
     server->deleteClient(fd);
 	server->setPfds(fd);
     return;
-}
-
-void removeUserFromChannels(Server *server, int fd)
-{
-    Client *c = server->GetClient(fd);
-    std::map<std::string, Channel *> chllist = server->GetChannelList();
-
-    for (std::map<std::string, Channel *>::iterator it = chllist.begin(); it != chllist.end(); it++ )
-    {
-        if (it->second->isMember(c->get_nickname()))
-        {
-            it->second->removeUser(c->get_nickname());
-            if (it->second->isOperator(c->get_nickname()))
-                it->second->removeOperator(c);
-            if (it->second->get_total_members() == 0 && it->second->get_channel_name()[0] != '!') //dont destroy safe channels starting with '!'
-                server->RemoveChannel(it->second->get_channel_name());
-        }
-    }
 }
