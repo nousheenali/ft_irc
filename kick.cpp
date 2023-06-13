35393127af3075c01477861b22361e556cfc7a6e
Sumaiya Fathima <79397105+Sumi111@users.noreply.github.com>
2023-06-13 15:29:51 +0400

    kick

--- kick.cpp
+++ kick.cpp
@@ -62,31 +62,40 @@ void kick(Server *server, int client_fd, msg_struct cmd_infos)
 {
     Client *client					= server->GetClient(client_fd);
 	std::vector<std::string> param 	= ft_split(cmd_infos.parameter, ' ');//param[0]->channel & param[1]->user list & param[2]->reason
+    if (param.empty())
+    {
+		server->SendReply(client_fd, ERR_NEEDMOREPARAMS(cmd_infos.cmd));
+		return ;
+	}
+	
 	std::map<std::string, Channel *>ch_lst	= server->GetChannelList();
 	std::map<std::string, Channel *>::iterator it_ch = ch_lst.find(param[0]);
     std::vector<std::string> kicked_lst;
     std::vector<std::string> temp;
     std::string kicked_by = client->get_nickname();
-    std::string reason;
+    std::string reason = "";
     temp.push_back(kicked_by);
 	
 	// get reason
 	if (cmd_infos.parameter.find(':') != std::string::npos)
 		reason = cmd_infos.parameter.substr(cmd_infos.parameter.find(':'), cmd_infos.parameter.length());
-	else
+	else if (param.size() > 1)
 		reason = param[2];
 	reason = reason.empty() ? " :byeee!\n" : reason;
 	
 	//check for multiple users to be kicked out
-    int flag = param[1].find(',') ? 1 : 0;
-	if (flag == 1)
-		kicked_lst = ft_split(param[1], ',');
-	else
-		kicked_lst.push_back(param[1]);
-
+	int flag = 0;
+	if (param.size() >= 1)
+	{
+    	flag = param[1].find(',') ? 1 : 0;
+		if (flag == 1)
+			kicked_lst = ft_split(param[1], ',');
+		else
+			kicked_lst.push_back(param[1]);
+	}
 	//error handling
-    if ((param[0].empty() && param[0].find(':') != std::string::npos) || kicked_lst.empty())
-        server->SendReply(client_fd, ERR_NEEDMOREPARAMS(cmd_infos.cmd));
+    if ((param[0].find(':') != std::string::npos) || kicked_lst.empty())
+		server->SendReply(client_fd, ERR_NEEDMOREPARAMS(cmd_infos.cmd));
     else if (it_ch == ch_lst.end())
         server->SendReply(client_fd, ERR_NOSUCHCHANNEL(kicked_by, param[0]));
     else if (is_member(it_ch, kicked_lst) == FAILURE)
